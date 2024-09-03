ad003ecb0e8194f225b0b00d9a6e774cc9baa319
s4r4h4mdy0x01 <sara101hamdy@gmail.com>
2024-09-03 05:33:46 +0300

    Add the packages I used , Add api Constants

---
+++ lib/core/constants/string_manager.dart
@@ -0,0 +1,5 @@
+class StringManeger {
+  static const String error = 'Error';
+  static const String imageIsNullData =
+      'https://www.kindpng.com/picc/m/24-248253_user-profile-default-image-png-clipart-png-download.png';
+}
---
+++ lib/core/network/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiContants {
+  static const String baseUsrl = 'https://dummyjson.com/';
+  static const String home = 'products';
+}
